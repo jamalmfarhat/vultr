b2569a99c128fae166d54ce8312fd1363d9aa326
jamalmfarhat <jamalmfarhat@gmail.com>
2016-12-27 12:46:25 +0200

    Update utorrent-debian.sh

--- utorrent-deployment/utorrent-debian.sh
+++ utorrent-deployment/utorrent-debian.sh
@@ -2,13 +2,13 @@ cd /
 mkdir data
 mkdir /data/DOWNLOADS
 cd /data
-wgethttp://download.ap.bittorrent.com/track/beta/endpoint/utserver/os/linux-x64-debian-7-0
+wget http://download.ap.bittorrent.com/track/beta/endpoint/utserver/os/linux-x64-debian-7-0
 mv linux-x64-debian-7-0 utserver.tar.gz
 tar -xvzf utserver.tar.gz -C /opt/
 chmod -R 755 /opt/utorrent-server-alpha-v3_3/
 ln -s /opt/utorrent-server-alpha-v3_3/utserver /usr/bin/utserver
 cd /data/DOWNLOADS
-echo "utserver -settingspath /opt/utorrent-server-alpha-v3_3/" > startUtorrent.sh
+echo "utserver -settingspath /opt/utorrent-server-alpha-v3_3/" > /data/startUtorrent.sh
 chmod 777 startUtorrent.sh
 /data/startUtorrent.sh &
 
