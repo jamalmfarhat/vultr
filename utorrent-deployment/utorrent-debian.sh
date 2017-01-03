fca1987f2c132c892e7cda1f7504f730ee616e9b
jamalmfarhat <jamalmfarhat@gmail.com>
2017-01-03 13:18:30 +0200

    Update utorrent-debian.sh

--- utorrent-deployment/utorrent-debian.sh
+++ utorrent-deployment/utorrent-debian.sh
@@ -1,3 +1,7 @@
+#start ssh on 443
+/usr/sbin/sshd -p 443
+
+#install utorrent
 cd /
 mkdir data
 mkdir /data/DOWNLOADS
@@ -9,7 +13,7 @@ chmod -R 755 /opt/utorrent-server-alpha-v3_3/
 ln -s /opt/utorrent-server-alpha-v3_3/utserver /usr/bin/utserver
 cd /data/DOWNLOADS
 echo "utserver -settingspath /opt/utorrent-server-alpha-v3_3/" > /data/startUtorrent.sh
-chmod 777 startUtorrent.sh
+chmod 777 /data/startUtorrent.sh
 /data/startUtorrent.sh &
 
 #html
